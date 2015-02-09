f0a084e78b321ac1339254b545d29ede077e3b72
Lane Schultz <Lane Schultz@10.30.19.235>
2015-02-09 16:11:20 -0600

    Final Commit.  Figured out that I did not need anything in the child classes.

--- src/lab1/CollegeProgramingCourse.java
+++ src/lab1/CollegeProgramingCourse.java
@@ -5,10 +5,60 @@
  */
 package lab1;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Lane Schultz
  */
-public interface CollegeProgramingCourse {
+public abstract class CollegeProgramingCourse {
+    private String courseName;
+    private String courseNumber;
+    private double credits;
+   
+    //Setters for the Course Number, Course Name, & Credits
+    public final void setCourseNumber(String courseNumber) {
+        if(courseNumber == null || courseNumber.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseNumber cannot be null of empty string");
+            System.exit(0);
+        }
+        this.courseNumber = courseNumber;
+    }
     
+    public final void setCourseName(String courseName) {
+        if(courseName == null || courseName.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseName cannot be null of empty string");
+            System.exit(0);
+        }
+        this.courseName = courseName;
+    }
+    public final void setCredits(double credits) {
+        if(credits < 0.5 || credits > 4.0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: credits must be in the range 0.5 to 4.0");
+            System.exit(0);
+        }
+        this.credits = credits;
+    }
+    
+    //Getters for the Course Name, Course Number, and Credits
+    public final String getCourseName() {
+        return courseName;
+    }
+    
+    public final String getCourseNumber() {
+        return courseNumber;
+    }
+    
+    public final double getCredits() {
+        return credits;
+    }
+    
+    //Returns a capitalized version of the course name
+
+    public final String getCapitalizedCourseName() {
+        return getCourseName().toUpperCase();
+    }  
 }
--- src/lab1/IntroJavaCourse.java
+++ src/lab1/IntroJavaCourse.java
@@ -1,51 +1,34 @@
 package lab1;
 
+import javax.swing.JOptionPane;
+
 /**
  * Describe responsibilities here.
  *
  * @author      your name goes here
  * @version     1.00
  */
-public class IntroJavaCourse {
-    String courseName;
-    private String courseNumber;
-    private double credits;
+public class IntroJavaCourse extends CollegeProgramingCourse {
+    
     private String prerequisites;
-
+    
     public IntroJavaCourse(String courseName, String courseNumber) {
-        this.courseName = courseName;
-        this.courseNumber = courseNumber;
+        this.setCourseName(courseName);
+        this.setCourseNumber(courseNumber);
     }
-
-    public String getCourseNumber() {
-        return courseNumber;
-    }
-
-    public void setCourseNumber(String courseNumber) {
-        this.courseNumber = courseNumber;
-    }
-
-    public double getCredits() {
-        return credits;
-    }
-
 
     public String getPrerequisites() {
         return prerequisites;
     }
 
     public void setPrerequisites(String prerequisites) {
+        if(prerequisites == null || prerequisites.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: prerequisites cannot be null of empty string");
+            System.exit(0);
+        }
         this.prerequisites = prerequisites;
     }
 
-        public void setCredits(double credits) {
-        if(credits < 0 || credits > 5.0) {
-            System.out.println(
-                    "Error: credits must be in the range 0.5 to 4.0");
-            System.exit(0);
-        }
-        this.credits = credits;
-        //this.setCredits(credits);
-    }
-
+    
 }
