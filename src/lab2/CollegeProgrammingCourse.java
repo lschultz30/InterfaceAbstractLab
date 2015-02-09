1346bb41234dccf958a13aac1df760474da6a825
Lane Schultz <Lane Schultz@10.30.19.235>
2015-02-09 15:02:18 -0600

    Added an abstract class in lab one and an interface in lab 2.

---
+++ src/lab2/CollegeProgrammingCourse.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+/**
+ *
+ * @author Lane Schultz
+ */
+public interface CollegeProgrammingCourse {
+   public void setCredits(double credits);
+    
+    public void setCourseName(String courseName);
+    
+    public void setCourseNumber(String courseNumber);
+    
+    public double getCredits();
+    
+    public String getCourseNumber();
+    
+    public String getCourseName();
+    
+    
+}
